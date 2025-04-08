048dd03a8f6f0397c5c37ccbe94eba4c264541df
Skilvingr <skilvingr@gmail.com>
2025-04-08 12:32:57 +0200

    use divan instead of criterion

---
+++ benches/main.rs
@@ -0,0 +1,51 @@
+#![cfg(bench)]
+
+use cepstrum_extractor::{CepstrumExtractor, RealToComplex};
+use cepstrum_extractor::num_complex::Complex;
+
+
+const BUF_LEN: usize = 4096;
+const WIN_LEN: usize = 128;
+
+fn main() {
+    divan::main();
+}
+
+fn prepare() -> (Vec<Complex<f32>>, CepstrumExtractor<f32>) {
+    (
+        vec![0f32; BUF_LEN].to_complex_vec(),
+        CepstrumExtractor::new(WIN_LEN)
+    )
+}
+
+#[divan::bench(sample_size = 1000)]
+fn rceps_mut(b: divan::Bencher) {
+    let (mut buf, extractor) = prepare();
+    b.bench_local(|| {
+        extractor.rceps_mut(&mut buf);
+    });
+}
+
+#[divan::bench(sample_size = 1000)]
+fn rceps(b: divan::Bencher) {
+    let (buf, extractor) = prepare();
+    b.bench_local(|| {
+        divan::black_box(extractor.rceps_to_vec(&buf));
+    });
+}
+
+#[divan::bench(sample_size = 1000)]
+fn cceps_mut(b: divan::Bencher) {
+    let (mut buf, extractor) = prepare();
+    b.bench_local(|| {
+        extractor.cceps_mut(&mut buf);
+    });
+}
+
+#[divan::bench(sample_size = 1000)]
+fn cceps(b: divan::Bencher) {
+    let (buf, extractor) = prepare();
+    b.bench_local(|| {
+        divan::black_box(extractor.cceps_to_vec(&buf));
+    });
+}
